7d45af414147a73d47d3cc244229fefc3f4d250e
GianaS <gianams28@gmail.com>
2020-09-18 23:22:48 -0400

    italicized book title on about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -21,7 +21,7 @@ const TextWrapper = styled.p`
   padding-top: 15px;
 `
 
-const ABOUT_TEXT: JSX.Element = <TextWrapper>Janelle Solviletti is a twenty-five-year-old writer from the outskirts of Boston. She graduated from Marist College in Poughkeepsie, New York, with a Masters Degree in Marketing. The Cameo is her debut poetry book, which encapsulates the tensions between time and love and their competing entities. She believes that it is the visionary who can call a moment into existence without the help or hindrance of the hands of time. She hopes those who delve into her poetry will recollect moments and commemorate those relationships with others, the natural world, passions and past times; whatever it may be that was once taken for granted by time’s natural order.</TextWrapper>
+const ABOUT_TEXT: JSX.Element = <TextWrapper>Janelle Solviletti is a twenty-five-year-old writer from the outskirts of Boston. She graduated from Marist College in Poughkeepsie, New York, with a Masters Degree in Marketing. <i>The Cameo</i> is her debut poetry book, which encapsulates the tensions between time and love and their competing entities. She believes that it is the visionary who can call a moment into existence without the help or hindrance of the hands of time. She hopes those who delve into her poetry will recollect moments and commemorate those relationships with others, the natural world, passions and past times; whatever it may be that was once taken for granted by time’s natural order.</TextWrapper>
 
 const About = (): JSX.Element => {
   return (
