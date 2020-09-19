7dd6d7c020de10af78fb8cc35b13f1f9a7e22b72
GianaS <gianams28@gmail.com>
2020-09-18 23:15:23 -0400

    fix padding on about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,7 +5,7 @@ import Header from '../components/Header'
 import { fonts } from '../styles/styles'
 
 const AboutWrapper = styled.div`
-  padding: 40px 30% 0 30%;
+  padding: 35px 30% 0 30%;
 `
 
 const PhotoTextWrapper = styled.div`
@@ -18,7 +18,7 @@ const TextWrapper = styled.p`
   font-family: ${fonts.montserrat};
   line-height: 1.7;
   font-size: 16px;
-  padding-top: 30px;
+  padding-top: 15px;
 `
 
 const ABOUT_TEXT: JSX.Element = <TextWrapper>Janelle Solviletti is a twenty-five-year-old writer from the outskirts of Boston. She graduated from Marist College in Poughkeepsie, New York, with a Masters Degree in Marketing. The Cameo is her debut poetry book, which encapsulates the tensions between time and love and their competing entities. She believes that it is the visionary who can call a moment into existence without the help or hindrance of the hands of time. She hopes those who delve into her poetry will recollect moments and commemorate those relationships with others, the natural world, passions and past times; whatever it may be that was once taken for granted by time’s natural order.</TextWrapper>
