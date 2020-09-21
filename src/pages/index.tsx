3d172448451d0547bc3f26af04abd455edd75d7b
GianaS <gianams28@gmail.com>
2020-09-21 18:51:34 -0400

    removed unused video wrapper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,7 @@
 import React, { useState, useLayoutEffect } from 'react'
-import styled from 'styled-components'
 
 import video from '../assets/videos/cameoPromo1.mp4'
 import Seo from '../components/Seo'
-
-const VideoWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  padding-top: 0;
-  margin-top: 0;
-  
-  video {
-    width: 100% !important;
-    height: auto !important;
-  }
-`
 
 const Home = (): JSX.Element => {
   return (
