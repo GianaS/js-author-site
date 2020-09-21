3739fd004be548e8fbff4cd6689821e9872c4839
GianaS <gianams28@gmail.com>
2020-09-20 22:30:40 -0400

    video player is no longer dependent on height of nav bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,25 +9,16 @@ const VideoWrapper = styled.div`
   justify-content: center;
   padding-top: 0;
   margin-top: 0;
-  width: 100%;
-
-  ${({ headerHeight }: { headerHeight: number }) => (
-    `height: calc(100vh - ${headerHeight});`
-  )} 
+  
+  video {
+    width: 100%;
+    height: auto;
+  }
 `
 
 const Home = (): JSX.Element => {
-  const [headerHeight, setHeaderHeight] = useState('0px')
-
-  useLayoutEffect(() => {
-    const header = typeof document !== 'undefined' && document.getElementById('header-wrapper')
-    if (typeof window !== 'undefined' && header) {
-      setHeaderHeight(window.getComputedStyle(header, null).getPropertyValue("height"))
-    }
-  }, [])
-
   return (
-    <VideoWrapper headerHeight={headerHeight}>
+    <VideoWrapper>
       <Seo title='Home | Janelle Solviletti' />
       <video autoPlay muted loop >
         <source src={video} type='video/mp4' />
