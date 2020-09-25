37c5e4d85fff6a0076c7891bfc6f657750833b63
GianaS <gianams28@gmail.com>
2020-09-25 12:37:17 -0400

    refactored cameo page to remove tech debt

--- src/pages/cameo.tsx
+++ src/pages/cameo.tsx
@@ -15,6 +15,15 @@ const PageWrapper = styled.div`
   background-image: url(${roseImage});
   background-size: contain;
   background-repeat: no-repeat;
+
+  iframe {
+    box-shadow: 2px 4px 15px ${colors.grey};
+    margin-left: 40px;
+
+    @media (max-width: 700px) {
+      margin-left: 0;
+    }
+  }
 `
 
 const Title = styled.h1`
@@ -23,7 +32,7 @@ const Title = styled.h1`
   padding-bottom: 20px;
 `
 
-const Flexbox = styled.div`
+const Section = styled.div`
   display: flex;
   padding-bottom: 80px;
 
@@ -53,11 +62,19 @@ const Text = styled.p`
   padding-bottom: 10px;
 `
 
-const RightColumn = styled.div`
+const TextButtonWrapper = styled.div`
   padding-left: 40px;
 
   @media (max-width: 700px) {
     padding-left: 0;
+  }
+`
+
+const ButtonWrapper = styled.div`
+  padding-bottom: 20px;
+
+  @media (max-width: 700px) {
+    padding: 10px 0 30px 0;
   }
 `
 
@@ -78,6 +95,7 @@ const StyledLink = styled.a`
 
 const Cameo = (): JSX.Element => {
   const bookDescription: JSX.Element = <Text>“I wish to disunite the postulation that love and time are one and the same.” <i>The Cameo</i> is a short collection of poetry and prose depicting the damage of two souls intertwining, as well as the even bolder proposition that romantic disillusion itself is a mirage. A metaphysical investigation into desire, disorder, and the natural world.</Text>
+  const playlistDescription: JSX.Element = <Text>Music transcends through time. Over the years, these songs were part of the making of <i>The Cameo</i>,  and whisper secrets in the margins. Before you turn the page, tune in to the place where it all began.</Text>
   const amazonLink: string = 'https://www.amazon.com/dp/B08JLXYL38?ref_=pe_3052080_397514860'
 
   const navBreakpoint = typeof window !== 'undefined'
@@ -99,22 +117,23 @@ const Cameo = (): JSX.Element => {
     <PageWrapper headerHeight={headerHeight}>
       <Seo title='The Cameo | Janelle Solviletti' />
       <Title>The Cameo</Title>
-      <Flexbox>
+      <Section>
         <ImageWrapper>
           <img
             src={coverImage}
             alt='cameo book cover'
           />
         </ImageWrapper>
-        <RightColumn>
+        <TextButtonWrapper>
           {!navBreakpoint ? bookDescription : null}
-          <div style={{ paddingBottom: '20px' }}><StyledLink href={amazonLink}>Order now</StyledLink></div>
+          <ButtonWrapper>
+            <StyledLink href={amazonLink}>Order now</StyledLink>
+          </ButtonWrapper>
           {navBreakpoint ? bookDescription : null}
-        </RightColumn>
-      </Flexbox>
-      <Flexbox>
-        <Text>
-          Music transcends through time. Over the years, these songs were part of the making of The Cameo,  and whisper secrets in the margins. Before you turn the page, tune in to the place where it all began.          </Text>
+        </TextButtonWrapper>
+      </Section>
+      <Section>
+        {playlistDescription}
         <iframe
           src='https://open.spotify.com/embed/playlist/4zwUWpSsVgrRwAWObakMTw'
           width='300'
@@ -122,7 +141,7 @@ const Cameo = (): JSX.Element => {
           frameBorder='0'
           allow='encrypted-media'
         />
-      </Flexbox>
+      </Section>
     </PageWrapper>
   )
 }
