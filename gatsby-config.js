6700a23325bdd36e08a175555c6554e7cea0d1a9
GianaS <gianams28@gmail.com>
2021-05-13 21:57:15 -0400

    added gatsby-plugin-google-gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,6 +85,18 @@ module.exports = {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [272320896],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false
+        }
+      }
     }
   ]
 }
